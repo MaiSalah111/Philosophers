fff7de00acc1aada2b240d33225809dfd3520f3d
Mai Ahmed <maiahmed@lab1r4s9.42abudhabi.ae>
2024-07-09 10:52:43 +0400

    Philo

---
+++ libft_utils/ft_calloc.c
@@ -0,0 +1,14 @@
+
+#include "philo.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*tmp;
+
+	if (count == SIZE_MAX || size == SIZE_MAX)
+		return (NULL);
+	tmp = malloc(size * count);
+	if (!tmp)
+		return (NULL);
+	return (memset(tmp, 0, size * count));
+}
